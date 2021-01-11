1aacdde8e86a17a71d018852107c579cbe12ee8c
andrewmelnyk <melnyk.andrew@gmail.com>
2021-01-10 21:32:28 -0500

    Add files via upload

---
+++ src/App.styles.ts
@@ -0,0 +1,50 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        height: 100%;
+    }
+
+    body {
+        margin: 0;
+        padding: 0 20px;
+        display: flex;
+        justify-content: center;
+    }
+
+    * {
+        box-sizing: border-box;
+    }
+`;
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    > p {
+    }
+
+    .score {
+        font-size: 2rem;
+        margin: 0;
+    }
+
+    h1 {
+        filter: drop-shadow(2px 2px #0085a3);
+        text-align: center;
+        font-size: 70px;
+        margin: 20px;
+    }
+
+    .start, .next {
+        cursor: pointer;
+        border: 2px solid #d38558;
+        box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+        border-radius: 10px;
+        height: 40px;
+        margin: 20px 0;
+        padding: 0 40px;
+        text-align: center;
+    }
+`;
